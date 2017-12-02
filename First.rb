a694d419fa7cf307f0a8977a533522c9ab0d6863
hughferguson <hughferguson25@gmail.com>
2017-12-02 16:46:41 -0500

    Added things

--- First.rb
+++ First.rb
@@ -5,9 +5,15 @@ puts 'For double quotes, start with "single quotes"'
 ten = 10
 puts ten
 
+puts ten
 puts "ten ="
+puts "ten = #{ten}"
 
 # comments?
 
 puts "adding numbers in puts: #{8 * 4}"
 puts "order of operation in puts = #{(11 / 11) + (10 % 10)}"
+
+test_string = "test string"
+puts test_string
+puts "#{test_string}"
