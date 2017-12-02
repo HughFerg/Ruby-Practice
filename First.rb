81bdc20bf1ea9bfa970f7a48411f9fcca34741d9
hughferguson <hughferguson25@gmail.com>
2017-12-02 00:42:24 -0500

    first add

---
+++ First.rb
@@ -0,0 +1,13 @@
+puts "test"
+puts "testing ' unquote"
+puts 'For double quotes, start with "single quotes"'
+
+ten = 10
+puts ten
+
+puts "ten ="
+
+# comments?
+
+puts "adding numbers in puts: #{8 * 4}"
+puts "order of operation in puts = #{(11 / 11) + (10 % 10)}"
